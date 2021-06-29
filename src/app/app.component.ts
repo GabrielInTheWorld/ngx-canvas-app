1cf1f7d8b34f7399a8eeec44c7020c7eb33859e6
Gabriel Meyer <meyergabriel@live.de>
2021-06-29 22:01:24 +0200

    Adds fps-counter to <app />

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'canvas-app';
+    title = 'canvas-app';
+
+    public fps = 0;
+
+    private currentLoop = new Date();
+
+    public constructor() {
+        requestAnimationFrame(() => this.calculateFps());
+    }
+
+    private calculateFps(): void {
+        const nextLoop = new Date();
+        this.fps = Math.round(1000 / (+nextLoop - +this.currentLoop));
+        this.currentLoop = nextLoop;
+        requestAnimationFrame(() => this.calculateFps());
+    }
 }
